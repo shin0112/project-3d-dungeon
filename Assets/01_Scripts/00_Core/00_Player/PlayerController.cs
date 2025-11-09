ea87892667468d442029e9f05bb3468de6db8aa7
shin0112 <wnslcosltimo12@gmail.com>
2025-11-09 19:26:24 +0900

    feat: 플레이어 move 제작

--- Assets/01_Scripts/00_Core/00_Player/PlayerController.cs
+++ Assets/01_Scripts/00_Core/00_Player/PlayerController.cs
@@ -20,4 +20,36 @@ public class PlayerController : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    #region 플레이어 행동 로직
+    public void Move()
+    {
+        Vector3 direction =
+            transform.forward * _currentMovementInput.y +
+            transform.right * _currentMovementInput.x;
+        direction *= _moveSpeed;
+        direction.y = _rigidbody.velocity.y;
+
+        _rigidbody.velocity = direction;
+    }
+    #endregion
+
+    #region 플레이어 행동 입력
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            _currentMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            _currentMovementInput = Vector2.zero;
+        }
+    }
+
+    #endregion
 }
