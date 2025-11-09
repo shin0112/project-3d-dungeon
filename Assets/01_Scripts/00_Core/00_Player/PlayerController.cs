f595b31d7443f0fd3ec0462d3dcacf6c891a80ae
shin0112 <wnslcosltimo12@gmail.com>
2025-11-09 19:26:46 +0900

    feat: 플레이어 점프, 더블점프

--- Assets/01_Scripts/00_Core/00_Player/PlayerController.cs
+++ Assets/01_Scripts/00_Core/00_Player/PlayerController.cs
@@ -18,6 +18,8 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+
+        ResetJumpFlags();
     }
 
     private void FixedUpdate()
@@ -51,5 +53,54 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void OnJumpInpupt(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            CheckGround();
+            if (_canJump)
+            {
+                Logger.Log("점프");
+                _rigidbody.AddForce(Vector2.up * _jumpPower, ForceMode.Impulse);
+                _canJump = false;
+            }
+            else if (_canDoubleJump)
+            {
+                Logger.Log("더블 점프");
+                _rigidbody.AddForce(Vector2.up * _jumpPower, ForceMode.Impulse);
+                _canDoubleJump = false;
+            }
+        }
+
+    }
     #endregion
+
+    private void CheckGround()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) +(transform.up * 0.01f), Vector3.down)
+        };
+
+        foreach (var ray in rays)
+        {
+            if (Physics.Raycast(ray, 0.1f, _groundLayerMask))
+            {
+                _canJump = true;
+                _canDoubleJump = true;
+                return;
+            }
+        }
+
+        _canJump = false;
+    }
+
+    private void ResetJumpFlags()
+    {
+        _canJump = true;
+        _canDoubleJump = true;
+    }
 }
