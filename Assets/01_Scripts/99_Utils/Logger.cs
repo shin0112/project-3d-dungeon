9ff5fcd457637b47fe79422a07115296761ee940
shin0112 <wnslcosltimo12@gmail.com>
2025-11-09 19:26:58 +0900

    feat: custom logger 만들기

---
+++ Assets/01_Scripts/99_Utils/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public static class Logger
+{
+#if UNITY_EDITOR 
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    [System.Diagnostics.DebuggerStepThrough]
+    public static void Log(
+        string message,
+        [CallerFilePath] string filePath = "",
+        [CallerMemberName] string memberName = "")
+    {
+        string className = System.IO.Path.GetFileNameWithoutExtension(filePath);
+        Debug.Log($"[{className}.{memberName}] {message}");
+    }
+
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    [System.Diagnostics.DebuggerStepThrough]
+    public static void Warning(
+        string message,
+        [CallerFilePath] string filePath = "",
+        [CallerMemberName] string memberName = "")
+    {
+        string className = System.IO.Path.GetFileNameWithoutExtension(filePath);
+        Debug.LogWarning($"[{className}.{memberName}] {message}");
+    }
+#endif
+}
